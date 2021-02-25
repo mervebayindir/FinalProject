a40b31322279bee4f9f0d134178e6c420ea11efd
Merve Babayiğit <ceren_626@hotmail.com>
2021-02-25 23:01:01 +0300

    Proje dosyası ekle.

---
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -0,0 +1,64 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfProductDal : IProductDal
+    {
+        //NuGet
+        public void Add(Product entity)
+        {
+            //IDısposable pattern implementation of c#
+            using (NorthhwindContext context = new NorthhwindContext())
+            {
+                var addedEntity = context.Entry(entity);
+                addedEntity.State = EntityState.Added;
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(Product entity)
+        {
+            using (NorthhwindContext context = new NorthhwindContext())
+            {
+                var deletedEntity = context.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+        public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            using (NorthhwindContext context = new NorthhwindContext())
+            {
+                return context.Set<Product>().SingleOrDefault(filter);
+            }
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            using (NorthhwindContext context = new NorthhwindContext())
+            {
+                return filter == null 
+                    ? context.Set<Product>().ToList() 
+                    : context.Set<Product>().Where(filter).ToList();
+            }
+        }
+
+        public void Update(Product entity)
+        {
+            using (NorthhwindContext context = new NorthhwindContext())
+            {
+                var updatedEntity = context.Entry(entity);
+                updatedEntity.State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+    }
+}
